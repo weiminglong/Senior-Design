928ce96aa001844557d6cbe36f441e049466da29
larissaba <larissab1@gwu.edu>
2019-11-23 20:05:07 -0500

    TFIDF without stemming

--- backend/src/TFIDFfinal.py
+++ backend/src/TFIDFfinal.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import os,glob
+import os, glob
 import numpy as np
 import json
 from textblob import TextBlob
@@ -7,64 +7,60 @@ from textblob import TextBlob
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
-from scipy.sparse.csr import csr_matrix #need this if you want to save tfidf_matrix
+from scipy.sparse.csr import csr_matrix  # need this if you want to save tfidf_matrix
 
 from nltk.stem import PorterStemmer
 from nltk.tokenize import sent_tokenize, word_tokenize
 
-
-#path is that of the current directory
+# path is that of the current directory
 path = os.getcwd()
-#print(location)
+# print(location)
 
-#empty list of corpus
+# empty list of corpus
 corpus = []
 
 fullData = []
 fullData2 = []
-#append each file with .txt extension to the corpus
+# append each file with .txt extension to the corpus
 
 for filename in sorted(glob.glob(os.path.join(path, '*.txt'))):
     with open(filename, 'r') as f:
         text = f.read()
         corpus.append(text)
 
-vectorizer = TfidfVectorizer(analyzer='word',stop_words = 'english')
+vectorizer = TfidfVectorizer(analyzer='word', stop_words='english')
 tfidf_matrix = vectorizer.fit_transform(corpus)
 
-cv=CountVectorizer(analyzer='word', stop_words = 'english',lowercase=True)
+cv = CountVectorizer(analyzer='word', stop_words='english', lowercase=True)
 
 # this steps generates word counts for the words in your docs
-word_count_vector=cv.fit_transform(corpus)
+word_count_vector = cv.fit_transform(corpus)
 
 word_count_vector.shape
 
-
-tfidf_transformer=TfidfTransformer(smooth_idf=True)
+tfidf_transformer = TfidfTransformer()
 tfidf_transformer.fit(word_count_vector)
 
-
-count_vector=cv.transform(corpus)
-tf_idf_vector=tfidf_transformer.transform(count_vector)
+count_vector = cv.transform(corpus)
+tf_idf_vector = tfidf_transformer.transform(count_vector)
 
 feature_names = cv.get_feature_names()
 
 top5AllFiles = []
 
-
-#build dataframe of first document. Determined by the index od tf-idf_vector below
+# build dataframe of first document. Determined by the index od tf-idf_vector below
 
 corpusLength = len(corpus)
 
-for i in range(0,corpusLength):
-    #print(i)
-    df = pd.DataFrame(tf_idf_vector[i].T.todense(), index=feature_names, columns= ["tfidf"])
-    df.sort_values(by=["tfidf"],ascending=False)
-    #get top 5 words
-    top5=df.nlargest(5, "tfidf")
-    #print(top5)
+for i in range(0, corpusLength):
+    # print(i)
+    df = pd.DataFrame(tf_idf_vector[i].T.todense(), index=feature_names, columns=["tfidf"])
+    df.sort_values(by=["tfidf"], ascending=False)
+    # get top 5 words
+    top5 = df.nlargest(5, "tfidf")
+    # print(top5)
     array = []
-    data1=[]
+    data1 = []
     for i, j in top5.iterrows():
         data1.append(i)
         data1.append(j.tfidf)
@@ -72,19 +68,21 @@ for i in range(0,corpusLength):
     array.append(data1)
     top5AllFiles.append(array)
 
-#print(top5AllFiles)
-#print()
+# print(top5AllFiles)
+# print()
+
+listFiles = []
+
 
 def timeData(filename, listwords):
-
     tempfullData = []
-    foundWords= []
-        #try:
+    foundWords = []
+    # try:
     wordPresent = False
-    file = open(filename,"r")
-    read= file.readlines()
+    file = open(filename, "r")
+    read = file.readlines()
     file.close()
-    linkval=""
+    linkval = ""
     linktemp = []
     link = []
 
@@ -95,86 +93,114 @@ def timeData(filename, listwords):
             line = sentence.split()
             for each in line:
                 line2 = each.lower()
-                line2=line2.strip("!@#$%^&*(()_+=)")
-                line3=line2.split(":")
+                line2 = line2.strip("!@#$%^&*(()_+=)")
+                line3 = line2.split(":")
                 topword = []
-                if line3[0]=="word" and lower == line3[1].lower() and lower not in foundWords:
+                if line3[0] == "word" and lower == line3[1].lower() and lower not in foundWords:
                     temptopword = []
-                    name= lower
+                    name = lower
                     startTime = line[1]
                     endTime = line[2]
-                    val2=line3[1]
+                    val2 = line3[1]
                     foundWords.append(lower)
-                    topword = "word: "+val2+"  "+ startTime+"  "+ endTime
+                    topword = "word: " + val2 + "  " + startTime + "  " + endTime
                     temptopword.append(topword)
-                    tempfullData= tempfullData+temptopword
-                    wordPresent= True
-                    print()
-                if line3[0]=="link" and wordPresent == True and line[0] not in link:
+                    tempfullData = tempfullData + temptopword
+                    wordPresent = True
+                    # print(filename)
+                    # print(tempfullData)
+                    # print()
+                if line3[0] == "link" and wordPresent == True and line[0] not in link:
                     link.append(line[0])
 
-    tempfullData =  tempfullData + link
-    if(len(tempfullData)<=2):
+    # if(len(tempfullData) <5 and len(tempfullData) >0):
+    # continue
+    # print(tempfullData)
+    # print(listFiles)
+    # print()
+    tempfullData = tempfullData + link
+    if (len(tempfullData) <= 2):
+        tempfullData = []
+    if (len(tempfullData) < 5):
         tempfullData = []
     fullData.append(tempfullData)
 
+
+# print(listFiles)
 listwords = []
 listweights = []
 for i in top5AllFiles:
     size = len(i[0])
     temp = []
     tempfloat = []
-    for j in range(0,size):
-        tempVar=[]
+    for j in range(0, size):
+        tempVar = []
         tempWeight = []
-        if(j%2==0):
-            weight = i[0][j+1]
+        if (j % 2 == 0):
+            weight = i[0][j + 1]
             tempVar.append(i[0][j])
-            tempWeight.append(i[0][j+1])
-            temp = temp+ tempVar
-            tempfloat = tempfloat+tempWeight
+            tempWeight.append(i[0][j + 1])
+            temp = temp + tempVar
+            tempfloat = tempfloat + tempWeight
     listwords.append(temp)
     listweights.append(tempfloat)
 
-
 for i in listwords:
-    #parse through file and get time stamp
-      for filename in sorted(glob.glob(os.path.join(path, '*.csv'))):
-            timeData(filename,i)
+    # parse through file and get time stamp
+    for filename in sorted(glob.glob(os.path.join(path, '*.csv'))):
+        # print(filename)
+        timeData(filename, i)
 
-#stip empty list within a list
+# stip empty list within a list
 fullData2 = [e for e in fullData if e]
 
-#variable definition
+# variable definition
 dictCorpus = {}
+
+sizeI = len(fullData2[0]) - 1
 count = 0
-
-#print(fullData2)
-
-
+lengthLimit = len(listweights)
+# print("size")
+# print(sizeI)
+# print(len(listweights)
+print(sizeI)
 for i in fullData2:
-    myDict = {}
-    #print(i)
-    size = len(listweights[count])
-    #print(size)
-    #print(size)
     increment = 0
-    #print(i)
-    for j in range(0,size):
-        #print(len(i))
-        test = []
-        temper = []
-        temper.append(listweights[count][j])
-        #print(i[j])
-        test.append(i[j])
-        myDict[increment] =test+temper
-        if(j==size-1):
-            myDict[increment].append(i[size])
-        increment+=1
-    dictCorpus[count]=myDict
-    count+=1
+    myDict = {}
+    # print(count)
+    # print(i[count])
+    # print(listweights[count])
+    # print()
+    # print()
+    # print(len(listweights[count])
+    # if count < lengthLimit:
+    for j in range(0, sizeI):
+        if count < lengthLimit:
+            test = []
+            tester = []
+            # print(count)
+            # print(j)
+            # print(listweights[count][j])
+            # print(count)
+            tester.append(listweights[count][j])
+            # print(count)
+            test.append(i[j])
+            # print(listweights[count][j])
+            # print(i[j])
+            myDict[increment] = test + tester
+            # print(myDict[increment])
+            # print(myDict)
+            if (j == sizeI - 1):
+                myDict[increment].append(i[j])
+                continue
+            # print(i[j])
+            increment += 1
+    dictCorpus[count] = myDict
+    count += 1
 print(dictCorpus)
-
-#write to json file
+# store dictionary in json file
 with open('top5Words.json', 'w') as filehandle:
-        json.dump(dictCorpus, filehandle)
+    json.dump(dictCorpus, filehandle)
+# store json format in database
+#json.dump(dictCorpus, sort_keys=True, indent=5)
+
