eb47233be48615fb3ef9e9830d7f93a3880fe2ae
Weiming <longweiming@gwu.edu>
2020-01-19 19:32:32 -0500

    Added button and text link to jump to specific time in video

--- frontend/src/app/viewer/viewer.component.ts
+++ frontend/src/app/viewer/viewer.component.ts
@@ -7,11 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewerComponent implements OnInit {
 
-  videoURL = 'https://qa-classifier.s3.amazonaws.com/Taxonomy.mp4'
+
+  videoURL = 'https://qa-classifier.s3.amazonaws.com/Taxonomy.mp4';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  jumpTo10sec() {
+    const video = document.getElementById('video1');
+    video.currentTime = 10;
+  }
+
+    jumpTo(time) {
+    const video = document.getElementById('video1');
+    video.currentTime = time;
+  }
 }
