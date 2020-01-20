06cbe5558cc3d9b846657ec070738ae33c2880ad
Weiming <longweiming@gwu.edu>
2020-01-19 19:13:35 -0500

    Video viewer and alignment

--- frontend/src/app/viewer/viewer.component.ts
+++ frontend/src/app/viewer/viewer.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewerComponent implements OnInit {
 
+  videoURL = 'https://qa-classifier.s3.amazonaws.com/Taxonomy.mp4'
+
   constructor() { }
 
   ngOnInit() {
